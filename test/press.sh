f59b0c3fc059bb9ea041c6b247355e49f5f34478
sanbai <sanbai@mya105023.cm3>
2013-08-14 13:55:57 +0800

    add fio as pressure-testing tool

--- test/press.sh
+++ test/press.sh
@@ -1,18 +1,18 @@
-DEV=sdb
 
 run_io()
 {
-	for i in `seq 1 10`; do
-		dd if=/dev/$DEV of=/dev/null iflag=direct skip=$i count=1 bs=1M
-	done
+    fio test/pure.fio &
 }
 
 while [ 1 ]; do
 	make setup
 	run_io
-	cat /proc/io-latency/$DEV/io_latency_us
-	cat /proc/io-latency/$DEV/io_latency_ms
-	cat /proc/io-latency/$DEV/io_latency_s
-	echo "" > /proc/io-latency/$DEV/io_stats_reset
+    sleep 60
+    for DEV in sdb sdc sdd sde sdf sdg sdh sdi; do
+        cat /proc/io-latency/$DEV/soft_io_latency_us > /dev/null
+        cat /proc/io-latency/$DEV/read_io_latency_ms > /dev/null
+        cat /proc/io-latency/$DEV/io_latency_s > /dev/null
+        echo "1" > /proc/io-latency/$DEV/io_stats_reset
+    done
 	make unsetup
 done
